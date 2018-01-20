a47f14f748185e2acf1e6a5be495952bfea303bd
limeichen <2482698881@qq.com>
2018-01-20 19:30:11 +0800

    team

---
+++ js/index.js
@@ -0,0 +1,9 @@
+;(function (){
+	var W=document.documentElement.clientWidth||document.body.clientWidth;
+	document.documentElement.style.fontStyle=W/640*100+"px";
+	
+})()
+	
+	//var mask = mui.createMask(callback);//callback为用户点击蒙版时自动执行的回调；
+//mask.show();//显示遮罩
+//mask.close();//关闭遮罩
